2e87fa6f5889b9fa1023ef2d3f39e61e0ca6e90e
Insanus <insanus-mokrassar@yandex.ru>
2015-07-07 12:27:27 +0600

    Was created project at spring-boot

---
+++ src/main/java/it/sevenbits/Example/Application.java
@@ -0,0 +1,15 @@
+package it.sevenbits.Example;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @see <a href="https://spring.io/guides/gs/spring-boot/">Official guide</a>
+ */
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication app = new SpringApplication(Application.class);
+        app.setShowBanner(false);
+        app.run(args);
+    }
+}
