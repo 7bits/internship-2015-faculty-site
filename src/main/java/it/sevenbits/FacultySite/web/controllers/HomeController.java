7031be1e994f3f54e3d142afb48e596e0b2899b3
Ovsyannikov Aleksey <insanus-mokrassar@yandex.ru>
2015-08-13 14:43:39 +0600

    fixed displayed main

--- src/main/java/it/sevenbits/FacultySite/web/controllers/HomeController.java
+++ src/main/java/it/sevenbits/FacultySite/web/controllers/HomeController.java
@@ -1,5 +1,6 @@
 package it.sevenbits.FacultySite.web.controllers;
 
+import it.sevenbits.FacultySite.core.domain.contentOfPages.ContentDescription;
 import it.sevenbits.FacultySite.core.domain.gallery.AlbumDescription;
 import it.sevenbits.FacultySite.web.domain.contentOfPages.ContentDescriptionModel;
 import it.sevenbits.FacultySite.web.domain.gallery.ImageFromAlbumDescriptionModel;
@@ -35,13 +36,17 @@ public class HomeController {
     @RequestMapping(value = "/main")
     public String main(Model model) {
         List<ContentDescriptionModel> news = new ArrayList<>();
+        List<ContentDescription> mainInfo = new ArrayList<>();
         try {
             news = contentOfPagesService.getPagesWhichContainTypeWithCountWithPublish("News:%", (long) 5, true);
         }
         catch (Exception e){
             LOG.error(e.getMessage());
         }
+        mainInfo.add(new ContentDescription("Институт математики и иформационных технологий", "<h4>ОмГУ им. Ф. М. Достоевского</h4><h1>Институт математики и иформационных технологий</h1><p>ИМИТ (ранее – математический факультет) для тех, кто отличает аксиому от теоремы, для тех, кто любит решать трудные задачи и не боится их.</p><p>На нашем факультете прием абитуриентов и обучение студентов ведётся по направлению: «Прикладная математика и информатика».На младших курсах даётся фундаментальная подготовка, а на старших обучение ведётся по широкому спектру специализаций: системное программирование; математические методы защиты информации;  информационное обеспечение экономической деятельности и т.д..</p><p>Выпускники факультета легко найдут себе работу по специальности не только в нашем регионе, но и за его пределами, в том числе в США, Великобритании, Канаде или Германии. Спасибо за это нужно сказать нашим бывшим сотрудникам, преподавателям, выпускникам, в свое время перебравшимися за рубеж, но поддерживающим связь с факультетом. Проблем с конвертацией диплома выпускники факультета не испытывают.</p><p>Что немаловажно, у нас целых 90 бюджетных, то есть  совершенно бесплатных (для абитуриентов!) мест и целых 20 бюджетных мест в магистратуре.</p><p>Поступай и учись бесплатно на лучшем факультете города!</p><p>Деканат находится по адресу: Проспект Мира 55/а (первый корпус ОмГУ), каб. 217</p><p>Телефоны:  (3812) 64-42-38, (3812) 22-56-96</p><p>Часы работы: пн-пт с 9:00 до 17:00</p>", "", ""));
+        model.addAttribute("mainInfo", mainInfo);
         model.addAttribute("news", news);
+        model.addAttribute("title", "Последние новости");
         return "home/main";
     }
 
