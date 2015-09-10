3efe12afc7bf7a1c399c0b64e8804118d05dabfd
EremeevValery <veron.en55@gmail.com>
2015-09-10 15:02:00 +0700

    delete unused library

--- src/test/java/it/sevenbits/FacultySite/TestHeader/HeaderButtonContacts.java
+++ src/test/java/it/sevenbits/FacultySite/TestHeader/HeaderButtonContacts.java
@@ -9,7 +9,7 @@ import org.openqa.selenium.*;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.support.ui.Select;
 
-public class HeaderButtonContactsTest {
+public class HeaderButtonContacts {
     private WebDriver driver;
     private String baseUrl;
     private String newUrl;
